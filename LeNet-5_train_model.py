20ae332b82c99a89dc8ea4144557fc204e43b75f
zhangyt1234 <44441803+zhangyt1234@users.noreply.github.com>
2019-06-06 16:54:18 -0700

    Add files via upload

---
+++ LeNet-5_train_model.py
@@ -0,0 +1,108 @@
+# source code is from https://github.com/rom1504/tensorflow_captcha_solver
+import cv2
+import pickle
+import os.path
+import numpy as np
+from imutils import paths
+from sklearn.preprocessing import LabelBinarizer
+from sklearn.model_selection import train_test_split
+from keras.models import Sequential
+from keras.layers.convolutional import Conv2D, MaxPooling2D, AveragePooling2D
+from keras.layers.core import Flatten, Dense
+from helpers import resize_to_fit
+
+
+# LETTER_IMAGES_FOLDER = "extracted_letter_images"
+# MODEL_FILENAME = "captcha_model.hdf5"
+# MODEL_LABELS_FILENAME = "model_labels.dat"
+
+LETTER_IMAGES_FOLDER = "images"
+MODEL_FILENAME = "captcha_model3.hdf5"
+MODEL_LABELS_FILENAME = "model_labels3py.dat"
+
+# initialize the data and labels
+data = []
+labels = []
+
+# loop over the input images
+for image_file in paths.list_images(LETTER_IMAGES_FOLDER):
+    # Load the image and convert it to grayscale
+    try:
+        image = cv2.imread(image_file)
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    except:
+        continue
+
+    # Resize the letter so it fits in a 20x20 pixel box
+    image = resize_to_fit(image, 28, 28)
+
+    # Add a third channel dimension to the image to make Keras happy
+    image = np.expand_dims(image, axis=2)
+
+    # Grab the name of the letter based on the folder it was in
+    label = image_file.split(os.path.sep)[-2]
+
+    # Add the letter image and it's label to our training data
+    data.append(image)
+    labels.append(label)
+
+
+# scale the raw pixel intensities to the range [0, 1] (this improves training)
+data = np.array(data, dtype="float") / 255.0
+labels = np.array(labels)
+
+# Split the training data into separate train and test sets
+(X_train, X_test, Y_train, Y_test) = train_test_split(data, labels, test_size=0.25, random_state=0)
+
+# Convert the labels (letters) into one-hot encodings that Keras can work with
+lb = LabelBinarizer().fit(Y_train)
+Y_train = lb.transform(Y_train)
+Y_test = lb.transform(Y_test)
+
+# Save the mapping from labels to one-hot encodings.
+# We'll need this later when we use the model to decode what it's predictions mean
+with open(MODEL_LABELS_FILENAME, "wb") as f:
+    pickle.dump(lb, f)
+
+#Instantiate an empty model
+model = Sequential()
+
+# C1 Convolutional Layer
+model.add(Conv2D(6, kernel_size=(5, 5), strides=(1, 1), activation="tanh", input_shape=(28,28,1), padding="same"))
+
+# S2 Pooling Layer
+model.add(AveragePooling2D(pool_size=(2, 2), strides=(1, 1), padding="valid"))
+
+# C3 Convolutional Layer
+model.add(Conv2D(16, kernel_size=(5, 5), strides=(1, 1), activation="tanh", padding="valid"))
+
+# S4 Pooling Layer
+model.add(AveragePooling2D(pool_size=(2, 2), strides=(2, 2), padding="valid"))
+
+# C5 Fully Connected Convolutional Layer
+model.add(Conv2D(120, kernel_size=(5, 5), strides=(1, 1), activation="tanh", padding="valid"))
+#Flatten the CNN output so that we can connect it with fully connected layers
+model.add(Flatten())
+
+# FC6 Fully Connected Layer
+model.add(Dense(84, activation="tanh"))
+
+#Output Layer with softmax activation
+model.add(Dense(36, activation="softmax"))
+
+# Compile the model
+model.compile(loss="categorical_crossentropy", optimizer="SGD", metrics=["accuracy"])
+
+# evaluate the model
+
+test_score = model.evaluate(X_test, Y_test)
+print("Test loss {:.4f}, accuracy {:.2f}%".format(test_score[0], test_score[1] * 100))
+
+# Train the neural network
+model.fit(X_train, Y_train, validation_data=(X_test, Y_test), batch_size=32, epochs=10, verbose=1)
+
+# Save the trained model to disk
+model.save(MODEL_FILENAME)
+
+
+
